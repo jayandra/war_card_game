b18c9a35116e503a0fe3b89a0a8cc8a8f6f8362b
jayandra <jayandrapokharel@gmail.com>
2025-08-07 21:20:25 -0500

    minor

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 require 'minitest/autorun'
 require 'pry'
 
-# Add the lib directory to the load path
-$LOAD_PATH.unshift File.expand_path('../', __FILE__)
-
 # Require all the source files
 require_relative '../lib/cards'
 require_relative '../lib/player'
